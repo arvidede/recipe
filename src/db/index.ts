662e2045ebd12a7f9000d8f42c86383373359a29
Arvid Edenheim <arvidede@icloud.com>
2023-11-11 21:32:50 +0100

    disable cache in production

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,35 +7,51 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const REQUEST_CACHE_FILE = "requests.json"
 const REQUEST_CACHE_FILE_PATH = join(__dirname, "cache", REQUEST_CACHE_FILE)
 
+const IS_DEV = process.env.NODE_ENV === "development"
+
 const logger = getLogger("db")
 
 export default class DB {
     db!: Map<string, string>
     constructor() {
-        this.load()
-        this.listen()
+        this.init()
+    }
+
+    private init() {
+        if (IS_DEV) {
+            this.initDBFromFile()
+            this.listen()
+        } else {
+            this.initDB()
+        }
     }
 
     public set(key: string, payload: string) {
-        logger.info("SET", key)
         if (!this.db) {
-            this.load()
+            logger.error("SET", "DB not initialised")
+            return
         }
+
+        logger.info("SET", key)
         this.db.set(key, payload)
-        fs.writeFileSync(
-            REQUEST_CACHE_FILE_PATH,
-            JSON.stringify(Object.fromEntries(this.db)),
-            "utf-8"
-        )
+
+        if (IS_DEV) {
+            this.cache()
+        }
     }
 
-    private load() {
+    private initDBFromFile() {
         logger.info("LOAD")
+
         this.db = new Map<string, string>(
             Object.entries(
                 JSON.parse(fs.readFileSync(REQUEST_CACHE_FILE_PATH, "utf-8"))
             )
         )
+    }
+
+    private initDB() {
+        this.db = new Map<string, string>()
     }
 
     public get(key: string) {
@@ -48,10 +64,18 @@ export default class DB {
         return this.db.has(key)
     }
 
+    private cache() {
+        fs.writeFileSync(
+            REQUEST_CACHE_FILE_PATH,
+            JSON.stringify(Object.fromEntries(this.db)),
+            "utf-8"
+        )
+    }
+
     private listen() {
         fs.watchFile(REQUEST_CACHE_FILE_PATH, () => {
             logger.info(`DETECTED FILE CHANGE ${REQUEST_CACHE_FILE_PATH}`)
-            this.load()
+            this.initDBFromFile()
         })
     }
 }
