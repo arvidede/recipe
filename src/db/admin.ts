3ecf217c308c184a48b6f3ecd365082901d3443f
Arvid Edenheim <arvidede@icloud.com>
2025-02-19 12:03:06 +0100

    removes public keys

--- src/db/admin.ts
+++ src/db/admin.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 
 export default function getAdminClient() {
     return createClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_URL!,
         process.env.SUPABASE_SERVICE_KEY!,
         {
             auth: {
--- src/db/client.ts
+++ src/db/client.ts
@@ -6,8 +6,8 @@ let client: ReturnType<typeof createBrowserClient<Database>>
 export default function getBrowserClient() {
     if (!client) {
         client = createBrowserClient<Database>(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+            process.env.SUPABASE_URL!,
+            process.env.SUPABASE_ANON_KEY!,
         )
     }
     return client
