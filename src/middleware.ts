4833eec7d445126ee03f6b0c07e668b08fafdf3c
Arvid Edenheim <arvidede@icloud.com>
2025-02-19 16:36:48 +0100

    remove redundant redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
 import { getUser } from "@/db/server"
 import { NextResponse, type NextRequest } from "next/server"
 
-const PROTECTED: Record<string, boolean> = {
-    "": false,
-    "/": false,
-    "/login": false,
-    "/auth": false,
-    "/home": true,
-}
+// const PROTECTED: Record<string, boolean> = {
+//     "": false,
+//     "/": false,
+//     "/login": false,
+//     "/auth": false,
+//     "/home": true,
+// }
 
-async function shouldRedirect(request: NextRequest) {
-    const user = await getUser()
+// async function shouldRedirect(request: NextRequest) {
+//     const user = await getUser()
 
-    return !user && PROTECTED[request.nextUrl.pathname]
-}
+//     return !user && PROTECTED[request.nextUrl.pathname]
+// }
 
 async function updateSession(request: NextRequest) {
     const supabaseResponse = NextResponse.next({
@@ -23,24 +23,13 @@ async function updateSession(request: NextRequest) {
     // IMPORTANT: Avoid writing any logic between createServerClient and
     // supabase.auth.getUser(). A simple mistake could make it very hard to debug
     // issues with users being randomly logged out.
-    if (await shouldRedirect(request)) {
-        const url = request.nextUrl.clone()
-        url.pathname = "/login"
-        return NextResponse.redirect(url)
-    }
+    // if (await shouldRedirect(request)) {
+    //     const url = request.nextUrl.clone()
+    //     url.pathname = "/login"
+    //     return NextResponse.redirect(url)
+    // }
 
-    // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-    // creating a new response object with NextResponse.next() make sure to:
-    // 1. Pass the request in it, like so:
-    //    const myNewResponse = NextResponse.next({ request })
-    // 2. Copy over the cookies, like so:
-    //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-    // 3. Change the myNewResponse object to fit your needs, but avoid changing
-    //    the cookies!
-    // 4. Finally:
-    //    return myNewResponse
-    // If this is not done, you may be causing the browser and server to go out
-    // of sync and terminate the user's session prematurely!
+    await getUser()
 
     return supabaseResponse
 }
