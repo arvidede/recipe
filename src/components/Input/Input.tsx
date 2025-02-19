260de21b8a90e3e4de5a6dc53ae793ba4f910cc3
Arvid Edenheim <arvidede@icloud.com>
2025-02-19 11:59:18 +0100

    Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,14 @@
+"use client"
+import clsx from "clsx"
+import { InputHTMLAttributes } from "react"
+import styles from "./Input.module.scss"
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    className?: string
+}
+
+function Input({ className, ...props }: Props) {
+    return <input {...props} className={clsx(styles.input, className)} />
+}
+
+export default Input
