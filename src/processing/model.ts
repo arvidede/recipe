15eafa1374134fbf6e8b59dbbaddc2ba4dd7d2ba
Arvid Edenheim <arvidede@icloud.com>
2025-02-20 14:11:04 +0100

    switch to gemini

--- src/processing/model.ts
+++ src/processing/model.ts
@@ -1,5 +1,7 @@
-import { openai } from "@ai-sdk/openai"
+// import { openai } from "@ai-sdk/openai"
+import { google } from "@ai-sdk/google"
 
-const model = openai("gpt-4o-mini", { structuredOutputs: true })
+// const model = openai("gpt-4o-mini", { structuredOutputs: true })
+const model = google("gemini-2.0-flash-001", { structuredOutputs: true })
 
 export default model
